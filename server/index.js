274717ea889b3b664c409321c53913523e18f48e
Nilesh <nileshtiwari@itgeeks.com>
2025-10-09 13:09:16 +0530

    project setup: react + node

--- server/index.js
+++ server/index.js
@@ -1,18 +1,23 @@
-const express = require("express");
-const path = require("path");
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const PORT = process.env.PORT || 5000;
 
-// Serve frontend build (for production)
+// Serve static frontend (after build)
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from Node backend!" });
 });
 
-// // Fallback for React Router
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-// });
+// React router fallback
+app.use('/{*any}', (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
