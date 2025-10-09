3f9608cec378c7478ecc499a9b12cd413505d19d
UtkarshloneyaITG <UtkarshLoniya>
2025-10-09 18:03:08 +0530

    update vercel.json

--- server/server.js
+++ server/server.js
@@ -1,25 +1,25 @@
-const path = require("path");
-const express = require("express");
-const dotenv = require("dotenv");
+    const path = require("path");
+    const express = require("express");
+    const dotenv = require("dotenv");
 
-const app = require("./src/index");
-const mongoConnect = require("./src/db/db");
-const errorHandler = require("./src/middleWare/errorHandler");
+    const app = require("./src/index");
+    const mongoConnect = require("./src/db/db");
+    const errorHandler = require("./src/middleWare/errorHandler");
 
-dotenv.config();
+    dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+    const PORT = process.env.PORT || 5000;
 
-mongoConnect();
+    mongoConnect();
 
-app.use(express.static(path.join(__dirname, "../client/dist")));
+    app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.use("/{*any}", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-});
+    app.use("/{*any}", (req, res) => {
+      res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+    });
 
-app.use(errorHandler);
+    app.use(errorHandler);
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    }); 
