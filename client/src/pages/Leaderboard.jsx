c44a29445783d6f7db12deb3bfd75cfe90482ce4
ParthsharmaITG <parthsharma@itgeeks.com>
2025-10-15 16:23:26 +0530

    add leaderboard file

---
+++ client/src/pages/Leaderboard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Leaderboard = () => {
+  return (
+    <main className='w-full h-screen'>
+      <div className='leader-board'>
+        <div className=''>
+            <div >
+                <div className='win-photo'>
+                    1
+                </div>
+                <div className='title'>
+                    <h2>1st Winner</h2>
+                </div>
+            </div>
+        </div>
+        <div></div>
+      </div>
+    </main>
+  )
+}
+
+export default Leaderboard
