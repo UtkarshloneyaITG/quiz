426d0cc1db234d9bc7f122435dc167409b4b6343
UtkarshloneyaITG <UtkarshLoniya>
2025-10-15 19:48:06 +0530

    submit differect type of question added

--- server/src/controller/answer.controller.js
+++ server/src/controller/answer.controller.js
@@ -7,48 +7,89 @@ const Que = require("../model/questionModel");
 
 // this function filter the answers and assign score to email
 async function setcorrect_answer(s_ans) {
-  const array_of_questions = await Que.find({}).lean();
-
-  let answers = s_ans.SubmitAnswers.filter((first_arr) =>
-    array_of_questions.some(
-      (sen_arr) =>
-        first_arr.QuestionID == sen_arr.QuestionID &&
-        first_arr.AnswerID == sen_arr.CorrectAnswerID
+  let score = 0;
+  let TypeTCO = s_ans.TypeTCO;
+  let TypeMCQ = s_ans.TypeMCQ;
+  const array_of_questions = questions;
+  const MCQ_que = array_of_questions.filter(
+    (value) => value.QuestionType == "mcq"
+  );
+  const TOC_que = array_of_questions.filter(
+    (value) => value.QuestionType == "tcq"
+  );
+  let answersTCO = TypeTCO.SubmitAnswers.filter((first_arr) =>
+    TOC_que.some(
+      (sec_arr) =>
+        first_arr.QuestionID == sec_arr.QuestionID &&
+        first_arr.AnswerID == sec_arr.CorrectAnswerID
     )
   );
+  const answersMCQ = TypeMCQ.SubmitAnswers.map((userQ) => {
+    const question = MCQ_que.find((q) => q.QuestionID === userQ.QuestionID);
+    if (!question) return null;
+    const correctAnswers = question.CorrectAnswerID;
+    const userAnswers = userQ.Answer;
+    let localScore = 0;
+    userAnswers.forEach((ans) => {
+      if (correctAnswers.includes(ans)) localScore += 0.5;
+      else localScore -= 0.5;
+    });
 
-  // score update system
-  const currentScore = answers.length * 5;
+    score += localScore;
+
+    return {
+      QuestionID: userQ.QuestionID,
+      userAnswers,
+      correctAnswers,
+      questionScore: localScore,
+    };
+  }).filter(Boolean);
+
+const simpleOBJ = answersMCQ.map((ele)=>{
+    return {
+        QuestionID : ele.QuestionID,
+        AnswerID : ele.correctAnswers
+    }
+})
+
+  score = score < 0 ? 0 + answersTCO.length : score + answersTCO.length;
+
+  if (score < 0) score = 0;
   const saveScore = await UserModel.updateOne(
     { email: s_ans.Email },
     {
       $push: {
         scoreHistory: {
           questionAttempt: {
-            correctAnswers: answers.length,
+            correctAnswers: answersTCO.length + simpleOBJ.length,
             attempt: s_ans.SubmitAnswers.length,
           },
-          score: currentScore,
+          score: score,
           esc_count: s_ans.esc_count,
         },
       },
     }
   );
-  // send all user data
   return {
     Email: s_ans.Email,
-    CorrectAnswers: answers,
-    Score: currentScore,
-    SubmitAnswers: s_ans.SubmitAnswers,
+    TypeTCO: {
+      SubmitAnswers: TypeTCO.SubmitAnswers,
+      CorrectAnswer: answersTCO,
+    },
+    TypeMCQ: {
+      SubmitAnswers: TypeMCQ.SubmitAnswers,
+      CorrectAnswer: simpleOBJ,
+    },
+    Score: score,
+    esc_count : s_ans.esc_count,
   };
 }
-
 const submitAnswers = async (req, res, next) => {
   try {
     const answerData = await setcorrect_answer(req.body);
     const ans_ = new Ans(answerData);
     const s_ans = await ans_.save();
-    res.status(200).json({     
+    res.status(200).json({
       message: "your answer submited successfully",
       Submits: req.body.SubmitAnswers.length,
     });
@@ -58,4 +99,3 @@ const submitAnswers = async (req, res, next) => {
 };
 
 module.exports = submitAnswers;
-
