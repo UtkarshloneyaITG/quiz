11cb0dc95a22c7534baf6a3d5f7d5955efb26446
balwansinghrajputITG <balwanrajput@itgeeks.com>
2025-10-15 14:45:09 +0530

    fix logout bug

--- client/src/pages/questionsType/AddSTQ.jsx
+++ client/src/pages/questionsType/AddSTQ.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
+import React from "react";
 
-function AddSTQ({question , setQuestion}) {
+function AddSTQ({ question, setQuestion }) {
   return (
-      <div>
-        <label>Question:</label>
-        <textarea
-          value={question}
-          onChange={(e) => setQuestion(e.target.value)}
-          rows={3}
-          className="w-full p-2 text-white border-2 border-black rounded-2xl outline-none"
-        />
-      </div>
-  )
+    <from>
+      <label>Question:</label>
+      <textarea
+        value={question}
+        onChange={(e) => setQuestion(e.target.value)}
+        rows={3}
+        className="w-full p-2 text-white border-2 border-black rounded-2xl outline-none"
+      />
+
+      <button
+        type="submit"
+        className="bg-purple-600 px-4 py-2 rounded hover:bg-purple-800 transition-colors duration-200 text-white"
+      >
+        Submit Question
+      </button>
+    </from>
+  );
 }
 
-export default AddSTQ
+export default AddSTQ;
